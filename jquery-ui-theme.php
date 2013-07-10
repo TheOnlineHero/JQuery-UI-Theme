bad99707ace724c6f8f2fc7c579b1dd287ccfcdb
No3x <noex_home@yahoo.de>
2013-07-10 14:31:19 +0200

    Codex conform enqueue to ensure correct integration with other plugins. Avoid "You're doing it wrong" Wordpress notices. Hook into init to ensure loading at the right time.

--- jquery-ui-theme.php
+++ jquery-ui-theme.php
@@ -21,27 +21,29 @@ Author: TheOnlineHero - Tom Skroza
 License: GPL2
 */
 
-//call register settings function
-if( !function_exists( 'jquery_ui_theme_enqueue_styles' ) ) {
-  function jquery_ui_theme_enqueue_styles() {
-    wp_register_style("jquery-ui", plugins_url("jquery-ui.min.css", __FILE__));
-    wp_register_style("jquery-ui-accordion", plugins_url("jquery.ui.accordion.min.css", __FILE__));
-    wp_register_style("jquery-ui-autocomplete", plugins_url("jquery.ui.autocomplete.min.css", __FILE__));
-    wp_register_style("jquery-ui-button", plugins_url("jquery.ui.button.min.css", __FILE__));
-    wp_register_style("jquery-ui-core", plugins_url("jquery.ui.core.min.css", __FILE__));
-    wp_register_style("jquery-ui-datepicker", plugins_url("jquery.ui.datepicker.min.css", __FILE__));
-    wp_register_style("jquery-ui-dialog", plugins_url("jquery.ui.dialog.min.css", __FILE__));
-    wp_register_style("jquery-ui-menu", plugins_url("jquery.ui.menu.min.css", __FILE__));
-    wp_register_style("jquery-ui-progressbar", plugins_url("jquery.ui.progressbar.min.css", __FILE__));
-    wp_register_style("jquery-ui-resizable", plugins_url("jquery.ui.resizable.min.css", __FILE__));
-    wp_register_style("jquery-ui-selectable", plugins_url("jquery.ui.selectable.min.css", __FILE__));
-    wp_register_style("jquery-ui-slider", plugins_url("jquery.ui.slider.min.css", __FILE__));
-    wp_register_style("jquery-ui-spinner", plugins_url("jquery.ui.spinner.min.css", __FILE__));
-    wp_register_style("jquery-ui-tabs", plugins_url("jquery.ui.tabs.min.css", __FILE__));
-    wp_register_style("jquery-ui-theme", plugins_url("jquery.ui.theme.min.css", __FILE__));
-    wp_register_style("jquery-ui-tooltip", plugins_url("jquery.ui.tooltip.min.css", __FILE__));
-  }
-  jquery_ui_theme_enqueue_styles();
+function register_styles() {
+	//call register settings function
+	if( !function_exists( 'jquery_ui_theme_enqueue_styles' ) ) {
+		function jquery_ui_theme_enqueue_styles() {
+			wp_register_style("jquery-ui", plugins_url("jquery-ui.min.css", __FILE__));
+			wp_register_style("jquery-ui-accordion", plugins_url("jquery.ui.accordion.min.css", __FILE__));
+			wp_register_style("jquery-ui-autocomplete", plugins_url("jquery.ui.autocomplete.min.css", __FILE__));
+			wp_register_style("jquery-ui-button", plugins_url("jquery.ui.button.min.css", __FILE__));
+			wp_register_style("jquery-ui-core", plugins_url("jquery.ui.core.min.css", __FILE__));
+			wp_register_style("jquery-ui-datepicker", plugins_url("jquery.ui.datepicker.min.css", __FILE__));
+			wp_register_style("jquery-ui-dialog", plugins_url("jquery.ui.dialog.min.css", __FILE__));
+			wp_register_style("jquery-ui-menu", plugins_url("jquery.ui.menu.min.css", __FILE__));
+			wp_register_style("jquery-ui-progressbar", plugins_url("jquery.ui.progressbar.min.css", __FILE__));
+			wp_register_style("jquery-ui-resizable", plugins_url("jquery.ui.resizable.min.css", __FILE__));
+			wp_register_style("jquery-ui-selectable", plugins_url("jquery.ui.selectable.min.css", __FILE__));
+			wp_register_style("jquery-ui-slider", plugins_url("jquery.ui.slider.min.css", __FILE__));
+			wp_register_style("jquery-ui-spinner", plugins_url("jquery.ui.spinner.min.css", __FILE__));
+			wp_register_style("jquery-ui-tabs", plugins_url("jquery.ui.tabs.min.css", __FILE__));
+			wp_register_style("jquery-ui-theme", plugins_url("jquery.ui.theme.min.css", __FILE__));
+			wp_register_style("jquery-ui-tooltip", plugins_url("jquery.ui.tooltip.min.css", __FILE__));
+		}
+		jquery_ui_theme_enqueue_styles();
+	}
 }
-
+add_action('init', 'register_styles');
 ?>
